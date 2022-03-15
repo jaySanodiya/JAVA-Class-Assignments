774b90fe9106e82dfd9019de847e455e7074ed64
Jay Sanodiya <jay.sanodiya09@gmail.com>
2022-03-15 22:16:32 +0530

    question 1 completed

--- src/com/week3/Tuesday/Question1/Main.java
+++ src/com/week3/Tuesday/Question1/Main.java
@@ -11,18 +11,52 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter num1");
-        int num1 = sc.nextInt();
 
-        System.out.println("Enter num2");
-        int num2 = sc.nextInt();
+        try{
+            int num1 = 0;
+            try{
+                num1 = sc.nextInt();
+            }
+            catch (Exception er){
+                System.out.println("Please enter valid number");
+            }
 
-        String message = null;
-        int num3 = num1 / num2;
-        if (num3 > 10) {
-            message = "Welcome to Exception Handling ";
+            System.out.println("Enter num2");
+            int  num2 = sc.nextInt();
+
+            try {
+                if(num2==0){
+                    ArithmeticException Ae = new ArithmeticException("");
+                    throw Ae;
+                }
+
+            }catch (Exception er){
+                System.out.println("num2 should not be 0");
+
+            }
+
+            String message = null;
+            int num3 = num1 / num2;
+
+            try{
+                if (num3 > 10) {
+                    message = "Welcome to Exception Handling ";
+                    System.out.println("Message is :" + message.toUpperCase());
+                }
+                else {
+                    Exception ex = new Exception("");
+                    throw ex;
+                }
+            }
+            catch (Exception er){
+
+                System.out.println("String value is null");
+
+            }
         }
-        System.out.println("Message is :" + message.toUpperCase());
-
+        catch (Exception error){
+            error.getMessage();
+        }
 
         System.out.println("end of main");
     }
